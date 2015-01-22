356858d594e4b25f112489a41cd9f61b65f6a5a4
alex31481 <alex.c.liang@gmail.com>
2015-01-22 00:06:21 -0500

    initial coding done
    need to turn this into a command line tool

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 'use strict';
 var browserSync = require('browser-sync');
+var path = require('path');
+
+
+process.argv.forEach(function(val,index,array){
+  console.log(index+':'+val);
+});
+
+var file = process.argv[2];
+var fileName = path.basename(file);
+var dirName=path.dirname(file);
+console.log('dir is '+file);
+console.log('filename is '+dirName);
 
 
 var config = {
   server: {
-    baseDir: 'app',
+    baseDir: [ 'app',dirName ],
     directory: false
   },
-  files: 'app/**',
-  browser: 'google chrome'
+  files: [ 'app/**',file ],
+  browser: 'google chrome',
+  startPath: '#/mdpreview/'+fileName 
 };
 
 browserSync(config, function(err) {
